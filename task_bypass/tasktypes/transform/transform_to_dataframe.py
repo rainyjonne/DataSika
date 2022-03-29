0548a11285c706d8cf35560b0a3834763dc62374
rainyjonne <rainyjonne@gmail.com>
2022-03-29 11:15:12 +0800

    update stage2 codes

--- task_bypass/tasktypes/transform/transform_to_dataframe.py
+++ task_bypass/tasktypes/transform/transform_to_dataframe.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import json
 from io import StringIO
 
 
@@ -7,5 +8,9 @@ def transform_to_dataframe(input_string, str_type):
 
     if str_type == "csv":
         result = pd.read_csv(StringIO(input_string), sep=",")
+
+    if str_type == "json":
+    	result = pd.DataFrame(json.loads(input_string))
+
 
     return result
