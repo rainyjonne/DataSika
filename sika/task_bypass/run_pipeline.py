58dd08a99e6eeb0bfb394dbdc93a8890e2d158d9
Soumya Ray <soumya.ray@gmail.com>
2022-06-24 17:17:25 +0800

    Moved run stage operation in to Stage class

--- sika/task_bypass/run_pipeline.py
+++ sika/task_bypass/run_pipeline.py
@@ -2,21 +2,11 @@
 from itertools import cycle
 from functools import reduce
 import sqlite3
-from sika.task_bypass.allocate_stage_tasks import allocate_stage_tasks
 from sika.task_bypass.stage import Stage
 
+# TODO: Refactor to make a Pipeline class?
 
-# REFACTOR TODO: make a Stage class: (1) execute stage (2) find last output of stage (...)
-
-# execute any particular stage
-def run_stage(stage, db):
-    done_stage = allocate_stage_tasks(stage, db)
-    # save dataframe to sqlite db
-    list(done_stage.values())[0][0].to_sql(name=stage['id'], con=db.returnConnection())
-    # update the done stage list
-    return done_stage
-
-def run_pipeline(stages, pipeline_name, db, restart_flag = False, done_stages={}):
+def run_pipeline(stages, pipeline_name, db, restart_flag = False):
     if restart_flag:
         stage_names = [ stage['id'] for stage in stages ]
         for stage_name in stage_names:
@@ -26,22 +16,25 @@ def run_pipeline(stages, pipeline_name, db, restart_flag = False, done_stages={}
     for stage_dict in stages:
         stage = Stage(stage_dict)
 
-        # check if it has something input from another stage
-        if stage.has_antecedent():
-            upstream_stages = stage.antecedents()
+        # TODO NEXT: See if all these commented lines are needed for any of the tests
+        
+        # # check if it has something input from another stage
+        # if stage.has_antecedent():
+        #     upstream_stages = stage.antecedents()
             
-            # if it's a merge stage
-            if len(upstream_stages) > 1:
-                done_stage = run_stage(stage.stage_dict, db)
+        #     # if it's a merge stage
+        #     if len(upstream_stages) > 1:
+        #         done_stage = stage.run(db)
 
-            # if user wants to cut tasks into smaller stages
-            # need to add more codes to here in the future
-            else:
-                done_stage = run_stage(stage.stage_dict, db)
-        else:
-            done_stage = run_stage(stage.stage_dict, db)
+        #     # if user wants to cut tasks into smaller stages
+        #     # need to add more codes to here in the future
+        #     else:
+        #         done_stage = stage.run(db)
+        # else:
+        #     done_stage = stage.run(db)
+        done_stage = stage.run(db)
 
-        done_stages.update({stage.name(): done_stage})
+        # done_stages.update({stage.name(): done_stage})
 
         # record done stage
         db.updatePipelineStatus(stage.name())
--- sika/task_bypass/stage.py
+++ sika/task_bypass/stage.py
@@ -1,3 +1,5 @@
+from sika.task_bypass.allocate_stage_tasks import allocate_stage_tasks
+
 # from sika.task_bypass.stage import Stage
 # stage = Stage({'from': 'somewhere'})
 # stage.has_antecedent()
@@ -6,13 +8,27 @@ class Stage:
     def __init__(self, stage_dict):
         self.stage_dict = stage_dict
 
+    # TODO: Refactor to make a stage data mapper for Yaml --> Stage object
+
     def has_antecedent(self):
         return True if 'from' in self.stage_dict else False
 
+    # TODO: Refactor to return a List of Stage objects
     def antecedents(self):
         return self.stage_dict['from']
 
     def name(self):
         return self.stage_dict['id']
+
+    # execute any particular stage
+    def run(self, db):
+        # TODO NEXT: Refactor make allocate stage tasks an instance method?
+        # TODO: Refactor done_stage either as an object of its own or have methods to inspect it
+        done_stage = allocate_stage_tasks(self.stage_dict, db)
+        # save output data dataframe to sqlite db
+        list(done_stage.values())[0][0].to_sql(name=self.name(), con=db.returnConnection())
+        # update the done stage list
+        return done_stage
+
 
  
