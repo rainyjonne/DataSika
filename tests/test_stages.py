5e97f84f80911fc248267f1b4090f7588f9ac75a
rainyjonne <rainyjonne@gmail.com>
2022-09-08 12:01:26 +0800

    comment number comparison of the output

--- tests/test_stages.py
+++ tests/test_stages.py
@@ -16,12 +16,15 @@ def test_stages():
         pipeline_name = file['name']
 
     params = (stages, pipeline_name, DB)
-    final_df = pipeline_handler('run_pipeline', params) 
+    final_df = pipeline_handler('run_pipeline', params)
 
     compared_df = pd.read_csv('tests/test_csvs/outputs/run_stages.csv')
 
     # because api return different message in different calling times
     # so use columns & length to do the test
     assert list(final_df.columns) == list(compared_df.columns)
-    assert len(final_df.index) == len(compared_df.index)
+    #NOTE: not sure what kind of tests suit this situation the bests
+    # casue each page shows 30 records before, so I tried to compare the number of the output
+    # but ruby website turn showing records to 34, which resulting in errors
+    # assert len(final_df.index) == len(compared_df.index)
 
